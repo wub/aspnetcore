83520bbd52fbeb4ed7140bca906e4eaa3ef82982
James Newton-King <james@newtonking.com>
2022-07-12 12:24:56 +0800

    HTTP/3: Don't wait for writes to complete in sends

--- src/Servers/Kestrel/Transport.Quic/src/Internal/QuicStreamContext.cs
+++ src/Servers/Kestrel/Transport.Quic/src/Internal/QuicStreamContext.cs
@@ -350,7 +350,7 @@ internal partial class QuicStreamContext : TransportConnection, IPooledStream, I
         Exception? shutdownReason = null;
         Exception? unexpectedError = null;
 
-        var sendCompletedTask = WaitForWritesCompleted();
+        _ = WaitForWritesCompleted();
 
         try
         {
@@ -447,7 +447,6 @@ internal partial class QuicStreamContext : TransportConnection, IPooledStream, I
         finally
         {
             ShutdownWrite(_shutdownWriteReason ?? _shutdownReason ?? shutdownReason);
-            await sendCompletedTask;
 
             // Complete the output after completing stream sends
             Output.Complete(unexpectedError);
